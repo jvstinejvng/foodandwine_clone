a45050ef556db6ab261b359f8cb29577adea8c3e
JustineJang <justinejang415@gmail.com>
2022-09-30 03:54:25 -0700

    recipe routes

---
+++ app/api/recipe_route.py
@@ -0,0 +1,40 @@
+from flask import Blueprint , request
+from flask_login import login_required, current_user
+from app.models import db, Recipe, User
+from app.forms import CreateRecipe
+from app.api.auth_routes import validation_errors_to_error_messages
+
+recipe_routes = Blueprint('recipes', __name__)
+
+# Get all recipes
+@recipe_routes.route('/')
+def all_recipes():
+    recipes = Recipe.query.all()
+    recipes_dict = {recipe.id:recipe.to_dict() for recipe in recipes}
+    return recipes_dict
+
+@recipe_routes.route('/', methods=['POST'])
+@login_required
+def add_user_recipe():
+  form = CreateRecipe()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  data = form.data
+
+  if form.validate_on_submit():
+    recipe = Recipe(
+        user_id=current_user.id,
+        title=data['title'],
+        description=data['description'],
+        img_url=data['img_url'],
+        total_time=data['total_time'],
+        servings=data['servings'],
+        ingredients=data['ingredients'],
+        directions=data['directions'],
+    )
+
+    db.session.add(recipe)
+    db.session.commit()
+
+    return recipe.to_dict()
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 400
