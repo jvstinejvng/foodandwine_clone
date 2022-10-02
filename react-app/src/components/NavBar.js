82d18d3f9b76c71e3bbc952bb8803eadc85d1fbe
JustineJang <justinejang415@gmail.com>
2022-10-01 23:24:54 -0700

    nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,61 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import breadandbutter from '../images/breadandbutterlogo.svg'
+import profileicon from '../images/profileicon.png'
+
+import './CSS/NavBar.css';
 
 const NavBar = () => {
+
+  // const sessionUser = useSelector(state => state.session.user)
+
+  // const [ showDropdown, setShowDropdown ] = useState(false)
+
+  // const handleDropdown = () => {
+  //   if (showDropdown) return
+  //   setShowDropdown(true)
+  // }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+  <>
+    <div className='NavBarMainDiv'>
+        <div className='NavBarTopContainer'>
+            <div className='NavBarLogo'>
+                  <img alt="bread and butter logo" src={breadandbutter} />
+            </div>
+            <div className='NavBarLoginHome'>
+                <span className='NavBarLogIn'>
+                    <img className='NavBarLogInImage' alt="Profile Icon" src={profileicon}/>
+                      <NavLink className='LogInLink' to='/login' exact={true} activeClassName='active'>
+                        Log In
+                      </NavLink>  
+                </span>   
+                <span className='NavBarHome'>
+                      <NavLink className='LogInLink' to='/' exact={true} activeClassName='active'>
+                        Home 
+                      </NavLink>  
+                </span>
+            </div>
+        </div>
+        <div className='NavBarBottomContainer'>
+            <div className='NavBarTags'>
+                  <NavLink className='NavBarBottomLink' to='/' exact={true} activeClassName='active'>
+                      HOME
+                  </NavLink>
+                  <NavLink className='NavBarBottomLink' to='/recipes' exact={true} activeClassName='active'>
+                      RECIPES
+                  </NavLink>
+                  <NavLink className='NavBarBottomLink' to='/new-recipe' exact={true} activeClassName='active'>
+                      ADD A RECIPE
+                  </NavLink>
+                  <NavLink className='NavBarBottomLink' to='/my-recipes' exact={true} activeClassName='active'>
+                      MY RECIPE
+                  </NavLink>
+            </div>
+        </div>
+    </div>
+  </>
   );
 }
 
