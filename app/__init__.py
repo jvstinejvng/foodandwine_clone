abf7a5e46ce761074d68cf7d6e2790a95b89efa4
JustineJang <justinejang415@gmail.com>
2023-02-13 14:56:05 -0800

    render

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+from flask import Flask, render_template, request, session, redirect
 from flask import Flask, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
