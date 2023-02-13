fa04afbb9c05ea170420973aa835edee463db8ff
JustineJang <justinejang415@gmail.com>
2023-02-13 15:16:28 -0800

    comment

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -3,6 +3,7 @@ from wtforms import StringField, IntegerField, DateTimeField
 from wtforms.validators import DataRequired, Length
 
 class CommentForm(FlaskForm):
+    
     rating = IntegerField('rating', validators=[DataRequired()])
     body = StringField('body')
     user_id = IntegerField('user', validators=[DataRequired()])
