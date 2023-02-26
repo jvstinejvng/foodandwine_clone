4f3995df7d853318aaeeb4a4f9d62274662bfe92
JustineJang <justinejang415@gmail.com>
2023-02-26 12:44:51 -0800

    render

--- app/models/db.py
+++ app/models/db.py
@@ -5,6 +5,7 @@ import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get('SCHEMA')
 
+
 db = SQLAlchemy()
 
 # add function to add a prefix to table names in production environment only
