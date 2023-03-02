d118634b99f51020878cafdadb8bc095bff9ef2a
JustineJang <justinejang415@gmail.com>
2023-03-01 22:24:50 -0800

    flask

--- react-app/src/components/Homepage.js
+++ react-app/src/components/Homepage.js
@@ -27,6 +27,7 @@ function Homepage() {
     }, [dispatch])
 
     
+    
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]; 
     const current = new Date();
     const date = `${month[current.getMonth()]} ${current.getDate()}, ${current.getFullYear()}`;
