6a80e1a3b5c4d342e076a24ad4e469725b8076d8
JustineJang <justinejang415@gmail.com>
2023-03-03 14:56:11 -0800

    mit

--- migrations/versions/750265cdfbdf_initial_migration.py
+++ migrations/versions/750265cdfbdf_initial_migration.py
@@ -7,7 +7,9 @@ Create Date: 2023-03-01 23:28:45.243554
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '750265cdfbdf'
@@ -84,6 +86,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('user_id', 'recipe_id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
+
     # ### end Alembic commands ###
 
 
--- migrations/versions/c4ff006df210_.py
+++ migrations/versions/c4ff006df210_.py
@@ -7,7 +7,9 @@ Create Date: 2023-03-02 20:05:29.175320
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'c4ff006df210'
@@ -19,6 +21,8 @@ depends_on = None
 def upgrade():
     pass
 
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
 
 def downgrade():
     pass
