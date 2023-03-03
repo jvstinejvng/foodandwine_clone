76de136b9cbb22235bf6bbf978a2360227cbaf76
JustineJang <justinejang415@gmail.com>
2023-03-02 20:06:01 -0800

    help

---
+++ migrations/versions/c4ff006df210_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c4ff006df210
+Revises: 750265cdfbdf
+Create Date: 2023-03-02 20:05:29.175320
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c4ff006df210'
+down_revision = '750265cdfbdf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
