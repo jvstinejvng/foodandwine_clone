c8add6d77c917ff7eb984ddf7f4cfc7243989186
JustineJang <justinejang415@gmail.com>
2023-03-09 14:22:41 -0800

    flask

--- app/models/ingredients.py
+++ app/models/ingredients.py
@@ -6,7 +6,6 @@ class Ingredient(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.String, nullable=False)
     food_stuff = db.Column(db.String(100), nullable=False)
