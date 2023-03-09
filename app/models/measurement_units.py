9f86f9cf2569ec9449e1a76259a7f2a02eea0cfe
JustineJang <justinejang415@gmail.com>
2023-03-09 14:32:08 -0800

    error at measurement table

--- app/models/measurement_units.py
+++ app/models/measurement_units.py
@@ -1,7 +1,10 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class MeasurementUnit(db.Model):
     __tablename__ = 'measurement_units'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     unit = db.Column(db.String(50), nullable=False)
